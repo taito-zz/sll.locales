b80f8a7754d25a5b7601a55d7e8eddffb120fe43
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-09-15 21:37:01 +0300

    Documentation updated and release 0.1.

---
+++ README.rst
@@ -0,0 +1,13 @@
+===========
+sll.locales
+===========
+
+This package is for overriding other packages' translations such as plone domain's.
+
+Change log
+----------
+
+0.1 (2012-09-15)
+================
+
+- Initial release. [taito]
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sll.locales',
-    version='0.0',
+    version='0.1',
     description="Conatains SLL locale files.",
     long_description=open("README.rst").read(),
     # Get more strings from
