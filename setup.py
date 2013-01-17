84f0c660cd4bcb8578279a1c26050c8908aea496
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-01-17 20:25:05 +0200

    Release 0.3

--- setup.py
+++ setup.py
@@ -4,29 +4,26 @@ from setuptools import setup
 
 setup(
     name='sll.locales',
-    version='0.2',
+    version='0.3',
     description="Overrides default translations of Plone for SLL, SLT and LL site.",
     long_description=open("README.rst").read(),
-    # Get more strings from
-    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 4.2",
-        "Framework :: Plone :: 4.3",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7"],
     keywords='',
     author='Taito Horiuchi',
     author_email='taito.horiuchi@abita.fi',
-    url='http://www.sll.fi/',
+    url='https://github.com/taito/sll.locales',
     license='None-free',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['sll'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'setuptools',
-        'zope.i18nmessageid'],
+        'Products.CMFPlone',
+        'setuptools'],
     entry_points="""
     # -*- Entry points: -*-
 
