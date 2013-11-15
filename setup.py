414f96caf9cf97e7e5227a65be49f4e33ce4de14
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-11-15 09:49:19 +0200

    release 0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sll.locales',
-    version='0.5.1',
+    version='0.6',
     description="Overrides default translations of Plone for SLL site.",
     long_description=open("README.rst").read(),
     classifiers=[
